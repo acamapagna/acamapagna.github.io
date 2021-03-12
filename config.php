2fc1f9c9a0e2ae90c96356d7062c38d43030a4f2
acamapagna <50273452+acamapagna@users.noreply.github.com>
2021-03-11 22:10:20 -0500

    .

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Discord Webhook Config
+$alert_level = 1; // 0 = None, 1 = @here, 2 = @everyone
+$embed_title = 'New Ban Appeal Submitted'; // Title in discord message embed.
+$embed_color = 0x6478C5; // Hex Code, no #. Leave 0x in front of hex code.
+$username = 'Form Submitted (from website)'; // Name to call webhook.
+$webhook_url = 'https://discord.com/api/webhooks/819357241936838667/Y6D1jCzfpV2RBuip-a4HCilRl6oDm2CDw-6ASUNoXL1sG3EMGjeTMhc4MOf4rnK55Ldz'; // Webhook url, guide https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks
+?>
